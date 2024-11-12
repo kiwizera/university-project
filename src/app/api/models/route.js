0b415cdaa574f449e249113ae5f02f59db5e54d4
João <63664299+kiwizera@users.noreply.github.com>
2024-11-12 20:41:28 -0300

    Delete src/app/api/models directory

--- src/app/api/models/route.js
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-export async function GET() {
-    const models = await prisma.produto.findMany();
-
-    return Response.json({ models });
-}
