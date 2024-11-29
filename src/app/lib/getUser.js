ddb27255348e222f826ff6b03f2fbf0612e5e62d
João <63664299+kiwizera@users.noreply.github.com>
2024-11-29 19:40:30 -0300

    Update getUser.js

--- src/app/lib/getUser.js
+++ src/app/lib/getUser.js
@@ -1,5 +1,5 @@
 export default async function getUser(id) {
-    const res = await fetch(`${process.env.SITE_URL}/api/users/${id}`);
+    const res = await fetch(`${env('SITE_URL')}/api/users/${id}`);
     const user = await res.json();
 
     return user
