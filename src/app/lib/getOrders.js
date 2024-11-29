e3794818cdc8ad55732524d9b0c0730cdc4d7700
João <63664299+kiwizera@users.noreply.github.com>
2024-11-29 19:56:31 -0300

    Update getOrders.js

--- src/app/lib/getOrders.js
+++ src/app/lib/getOrders.js
@@ -1,5 +1,6 @@
 export default async function getOrders(user_id) {
-    const res = await fetch(`${env('SITE_URL')}/api/orders/${user_id}`);
+    const site_url = process.env.SITE_URL;
+    const res = await fetch(`${site_url}/api/orders/${user_id}`);
     const orders = await res.json();
 
     return orders
