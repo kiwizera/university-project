d3e1c1af1d2e5036350f3b4001b6ccbeaa5d43d3
João <joaobizuca10@gmail.com>
2024-11-29 18:35:51 -0300

    release 1.1.1

--- src/app/lib/getOrders.js
+++ src/app/lib/getOrders.js
@@ -1,5 +1,5 @@
 export default async function getOrders(user_id) {
-    const res = await fetch(`http://localhost:3000/api/orders/${user_id}`);
+    const res = await fetch(`https://pi-3-eta.vercel.app//api/orders/${user_id}`);
     const orders = await res.json();
 
     return orders
--- src/app/lib/getUserFavorites.js
+++ src/app/lib/getUserFavorites.js
@@ -1,5 +1,5 @@
 export default async function getUserFavorites(id) {
-    const res = await fetch(`http://localhost:3000/api/favorites/${id}`);
+    const res = await fetch(`https://pi-3-eta.vercel.app//api/favorites/${id}`);
     const favorites = await res.json();
 
     return favorites
