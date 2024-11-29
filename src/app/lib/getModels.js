d79966111d8bc5b8d0ef07a78b6579c0399d6332
João <63664299+kiwizera@users.noreply.github.com>
2024-11-29 19:55:42 -0300

    Update getModels.js

--- src/app/lib/getModels.js
+++ src/app/lib/getModels.js
@@ -1,5 +1,6 @@
 export default async function getModel(id) {
-    const res = await fetch(`${process.env.SITE_URL}/api/models/${id}`);
+    const site_url = process.env.SITE_URL;
+    const res = await fetch(`${site_url}/api/models/${id}`);
     const model = await res.json();
 
     return model
